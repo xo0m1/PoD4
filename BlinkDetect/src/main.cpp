6599e6d938ae1573ed41c805574708d401f5d616
xo0m1 <pi@navio-rpi.(none)>
2015-11-01 11:20:59 -0500

    adding ADC module

--- BlinkDetect/src/main.cpp
+++ BlinkDetect/src/main.cpp
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include "../include/blink_detection.h"
-
+#include "../include/ads1015.h"
 
 using namespace std;
 
@@ -14,7 +14,44 @@ using namespace std;
 int main( int argc, char** argv )
 {
 	
-	blink_detection();
+	//blink_detection();
+	
+	
+	////////////////////////
+	
+	
+	ads1015_t adc;
+	int ret = ads1015_init(&adc);
+	if (!ret) 
+	{
+		cout << "ERROR initializing ADC" << endl;
+		return 0;
+	}
+	
+	
+	ret = ads1015_changeActiveChannel(&adc, 3);
+	if (ret < 0) 
+	{
+		cout << "ERROR changing channel in ADC" << endl;
+		return 0;
+	}
+	
+	
+	usleep(100);
+	
+	for(int i = 0; i < 1; i++)
+	{
+		float val = ads1015_getDataFromActiveChannel(&adc);
+		if (ret < 0) 
+		{
+			cout << "ERROR reading from ADC" << endl;
+			return 0;
+		}
+		cout << "read: " << val << " V" << endl;
+	}
+	ads1015_close(&adc);
+	
+	
 	
 	return 0;
 	
